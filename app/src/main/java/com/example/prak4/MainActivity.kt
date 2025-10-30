6935c6f716e8c391db274b0d6699046c35d25147
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-10-30 16:21:55 +0700

    memuat FormDataDiri

--- app/src/main/java/com/example/prak4/MainActivity.kt
+++ app/src/main/java/com/example/prak4/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.prak4.ui.theme.Prak4Theme
+import com.example.praktikum4.ui.theme.FormDataDiri
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Prak4Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FormDataDiri(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
