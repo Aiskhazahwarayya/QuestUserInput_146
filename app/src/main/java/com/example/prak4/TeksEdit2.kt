5921eb68e9d5535a46b3d2e66fe8ce18f8b27562
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-01 08:56:51 +0700

    menambahkan jarak  antar elemen

--- app/src/main/java/com/example/prak4/TeksEdit2.kt
+++ app/src/main/java/com/example/prak4/TeksEdit2.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -166,6 +167,7 @@ fun FormPendaftaran(modifier: Modifier = Modifier) {
                             .fillMaxWidth()
                             .height(60.dp)
                     )
+                    Spacer(modifier= Modifier.height(16.dp))
 
 
                 }
