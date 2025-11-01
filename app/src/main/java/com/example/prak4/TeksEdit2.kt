834e96daecce1c0136ff86f0855ed724677382a6
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-01 07:36:29 +0700

    Menambahkan composable FormPendaftaran

---
+++ app/src/main/java/com/example/prak4/TeksEdit2.kt
@@ -0,0 +1,9 @@
+package com.example.prak4
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormPendaftaran(modifier: Modifier = Modifier) {
+
+}
