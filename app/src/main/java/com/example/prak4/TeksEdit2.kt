cc18042b5ee75ca00ed618180721446d5af4a438
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-01 07:45:57 +0700

    menambahkan variabel warna

--- app/src/main/java/com/example/prak4/TeksEdit2.kt
+++ app/src/main/java/com/example/prak4/TeksEdit2.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun FormPendaftaran(modifier: Modifier = Modifier) {
@@ -23,6 +24,9 @@ fun FormPendaftaran(modifier: Modifier = Modifier) {
 
     val genderList = listOf("Laki-laki", "Perempuan")
     val statusList = listOf("Janda", "Lajang", "Duda")
+
+    val backgroundLavender = Color(0xFFF3E5F5)
+    val purpleButton = Color(0xFF7C3AED)
 
 
 }
