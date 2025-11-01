46e4ee80d540ee26f719892767b203610342056f
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-01 07:40:13 +0700

    Menambahkan textNama, textAlamat,textJK,dan textStatus

--- app/src/main/java/com/example/prak4/TeksEdit2.kt
+++ app/src/main/java/com/example/prak4/TeksEdit2.kt
@@ -1,9 +1,18 @@
 package com.example.prak4
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun FormPendaftaran(modifier: Modifier = Modifier) {
+
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
 
 }
